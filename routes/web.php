f2629f219d7ae9068c3ace1f943cdd3eb15d966f
Alec M <alecmattu1234@gmail.com>
2022-06-20 11:36:19 -0400

    Setup Bootstrap5, cleanup repo

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,42 @@
 <?php
+/*
+ * Produced: Mon Jun 20 2022
+ * Author: Alec M.
+ * GitHub: https://amattu.com/links/github
+ * Copyright: (C) 2022 Alec M.
+ * License: License GNU Affero General Public License v3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('login', function() {
+  if (Auth::check())
+    return redirect()->route('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+  return view('login');
+})->name("auth.login");
+
+Route::get('register', function() {
+  if (Auth::check())
+    return redirect()->route('home');
+
+  return view('register');
+})->name("register")->name("auth.register");
+
+Route::get('/', function() {
+  return view('home');
+})->middleware('auth')->name("home");
