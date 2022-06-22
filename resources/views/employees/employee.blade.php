9d64828d1735ca4f37ab5e4404636bb706f541ba
Alec M <alecmattu1234@gmail.com>
2022-06-22 18:22:43 -0400

    Create employee page

--- resources/views/employees/employee.blade.php
+++ resources/views/employees/employee.blade.php
@@ -13,13 +13,57 @@
     <div class="container-fluid">
       @include("partials.sidebar")
 
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3 bg-light">
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-3">
         @include("partials.errors")
 
-        <center>Employee...Details</center>
-        <pre>
-          {{print_r($employee)}}
-        </pre>
+        <div class="row">
+          <div class="col-xl-3 text-center mb-3">
+            <div class="card p-3 shadow-sm">
+              <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm mx-auto">
+              <h5 class="mb-0">{{$employee->firstname}} {{$employee->middlename}} {{$employee->lastname}}</h5>
+              <span class="badge bg-primary mt-1">{{$employee->title}}</span>
+              <span class="text-muted mt-1" title="{{$employee->hired_at}}">{{(new DateTime($employee->hired_at))->format("F jS, Y")}}</span>
+            </div>
+
+            <div class="card p-3 shadow-sm mt-3">
+              <div class="d-flex align-items-center justify-content-center text-muted mb-3">
+                <i class="fas fa-2xl fa-user-clock me-auto"></i>
+                <h4 class="card-title me-auto">Tenure</h4>
+              </div>
+              <h2 class="text-center">{{((new DateTime($employee->hired_at))->diff(new DateTime()))->d}} days</h2>
+            </div>
+
+            <div class="card p-3 shadow-sm mt-3">
+              <div class="d-flex align-items-center justify-content-center text-muted mb-3">
+                <i class="fas fa-2xl fa-money-bill me-auto"></i>
+                <h4 class="card-title me-auto">Salary</h4>
+              </div>
+              <h2 class="text-center">${{number_format($employee->pay_rate)}}</h2>
+            </div>
+
+            <div class="card p-3 shadow-sm mt-3">
+              <div class="d-flex align-items-center justify-content-center text-muted mb-3">
+                <i class="fas fa-2xl fa-calendar-check me-auto"></i>
+                <h4 class="card-title me-auto">Period Pay Units</h4>
+              </div>
+              <h2 class="text-center">
+                0
+                {{$employee->pay_type === "hourly" ? "hours" : "days"}}
+              </h2>
+            </div>
+          </div>
+          <div class="col-xl-9 mb-3">
+            <div class="card shadow-sm">
+              <div class="card-body">
+                <form method="POST" action="">
+                  @include("partials.employeeForm")
+                  <button type="submit" class="btn btn-primary">Save</button>
+                  <a class="btn text-danger" href="{{route("employees")}}">Cancel</a>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
 
---
+++ resources/views/partials/employeeForm.blade.php
@@ -0,0 +1,80 @@
+@csrf
+<div class="mb-3">
+  <label for="firstname" class="form-label">First Name</label>
+  <input type="text" class="form-control @error('title') is-invalid @enderror" name="firstname">
+</div>
+<div class="mb-3">
+  <label for="middlename" class="form-label">Middle Name</label>
+  <input type="text" class="form-control @error('title') is-invalid @enderror" name="middlename">
+</div>
+<div class="mb-3">
+  <label for="lastname" class="form-label">Last Name</label>
+  <input type="text" class="form-control @error('title') is-invalid @enderror" name="lastname">
+</div>
+<div class="mb-3">
+  <label for="email" class="form-label">Email Address</label>
+  <input type="email" class="form-control @error('title') is-invalid @enderror" name="email">
+  <div class="form-text">
+    This employee will not be granted access to this application. In order to grant access, you must create a separate user account for this employee.
+  </div>
+</div>
+<div class="mb-3">
+  <label for="telephone" class="form-label">Telephone</label>
+  <input type="text" class="form-control @error('title') is-invalid @enderror" name="telephone">
+</div>
+<div class="row g-3 mb-3">
+  <div class="col-12">
+    <label for="street1" class="form-label">Street #1</label>
+    <input type="text" class="form-control @error('title') is-invalid @enderror" name="street1" placeholder="1234 Main St">
+  </div>
+  <div class="col-12">
+    <label for="street2" class="form-label">Street #2</label>
+    <input type="text" class="form-control @error('title') is-invalid @enderror" name="street2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="col-md-6">
+    <label for="city" class="form-label">City</label>
+    <input type="text" class="form-control @error('title') is-invalid @enderror" name="city">
+  </div>
+  <div class="col-md-4">
+    <label for="state" class="form-label">State</label>
+    <select name="state" class="form-select @error('title') is-invalid @enderror">
+      <option value="MD" selected>Maryland</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="zip" class="form-label">Zip</label>
+    <input type="text" class="form-control @error('title') is-invalid @enderror" name="zip">
+  </div>
+</div>
+<div class="mb-3">
+  <label for="birthdate" class="form-label">Date of Birth</label>
+  <input type="date" class="form-control @error('title') is-invalid @enderror" name="birthdate">
+</div>
+<div class="mb-3">
+  <label for="hired_at" class="form-label">Date Hired</label>
+  <input type="date" class="form-control @error('title') is-invalid @enderror" name="hired_at">
+</div>
+<div class="mb-3">
+  <label for="pay_type" class="form-label">Pay Type</label>
+  <select class="form-control @error('title') is-invalid @enderror" name="pay_type">
+    <option value="hourly" selected>Hourly</option>
+    <option value="salary">Salary</option>
+  </select>
+</div>
+<div class="mb-3">
+  <label for="pay_period" class="form-label">Pay Period</label>
+  <select class="form-control @error('title') is-invalid @enderror" name="pay_period">
+    <option value="daily">Daily</option>
+    <option value="weekly">Weekly</option>
+    <option value="biweekly">Bi-Weekly</option>
+    <option value="monthly" selected>Monthly</option>
+  </select>
+</div>
+<div class="mb-3">
+  <label for="pay_rate" class="form-label">Salary</label>
+  <input type="number" class="form-control @error('title') is-invalid @enderror" name="pay_rate">
+</div>
+<div class="mb-3">
+  <label for="title" class="form-label">Job Title</label>
+  <input type="text" class="form-control @error('title') is-invalid @enderror" name="title">
+</div>
