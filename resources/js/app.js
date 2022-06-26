d9c034d29a063fa89dc8aa8ee63c54920f3c9633
Alec M <alecmattu1234@gmail.com>
2022-06-26 09:55:04 -0400

    Remove axios dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,11 @@
-import './bootstrap';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 
 window.Chart = Chart;
+
+window._ = require('lodash');
+
+try {
+    require('bootstrap');
+} catch (e) {}
