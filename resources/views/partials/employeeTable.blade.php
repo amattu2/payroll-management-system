a36e45eee585c261793d264f8614ca95f6689c27
Alec M <alecmattu1234@gmail.com>
2022-06-26 09:28:53 -0400

    Add link to employee

--- resources/views/partials/employeeTable.blade.php
+++ resources/views/partials/employeeTable.blade.php
@@ -8,21 +8,25 @@
         <th scope="col">Hire Date</th>
         <th scope="col">Pay Rate</th>
         <th scope="col">Status</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
       @forelse ($employees as $employee)
         <tr>
-          <td>{{str_pad($employee->id, 4, "0", STR_PAD_LEFT)}}</td>
-          <td>{{$employee->firstname}} {{$employee->lastname}}</td>
-          <td>{{$employee->email}}</td>
-          <td>{{$employee->hired_at}}</td>
-          <td>${{number_format($employee->pay_rate)}}</td>
-          <td>{{ucfirst($employee->employment_status)}}</td>
+          <td>{{ str_pad($employee->id, 4, '0', STR_PAD_LEFT) }}</td>
+          <td>{{ $employee->firstname }} {{ $employee->lastname }}</td>
+          <td>{{ $employee->email }}</td>
+          <td>{{ $employee->hired_at }}</td>
+          <td>${{ number_format($employee->pay_rate) }}</td>
+          <td>{{ ucfirst($employee->employment_status) }}</td>
+          <td>
+            <a href="{{ Route('employees.employee', $employee->id) }}" class="text-primary" role="button">View</a>
+          </td>
         </tr>
       @empty
         <tr>
-          <td class="text-center" colspan="6">No employees</td>
+          <td class="text-center" colspan="7">No employees</td>
         </tr>
       @endforelse
     </tbody>
