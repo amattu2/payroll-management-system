d0e4401fb6a2e0335ecaf1df38b7b3cf894b0d8c
Alec M <alecmattu1234@gmail.com>
2022-07-23 10:19:41 -0400

    Disable observer for employee factory

--- app/Console/Commands/Factories.php
+++ app/Console/Commands/Factories.php
@@ -26,42 +26,45 @@ use Illuminate\Console\Command;
 
 class Factories extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'run:factories';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'run:factories';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Run the database test factories';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Run the database test factories';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        // Delete old data
-        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        \App\Models\Employee::truncate();
-        \App\Models\Leave::truncate();
-        \App\Models\Timesheet::truncate();
-        \App\Models\TimesheetDay::truncate();
-        \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    // Delete old data
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    \App\Models\Employee::truncate();
+    \App\Models\Leave::truncate();
+    \App\Models\Timesheet::truncate();
+    \App\Models\TimesheetDay::truncate();
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
-        // Create new models
-        \App\Models\Employee::factory()->count(rand(6, 45))
-          ->has(\App\Models\Timesheet::factory()->count(rand(1, 8))
-            ->has(\App\Models\TimesheetDay::factory()->count(rand(1, 22))))
-          ->has(\App\Models\Leave::factory()->count(rand(1, 3)))
-          ->create();
+    // Disable Observer
+    \App\Models\Employee::unsetEventDispatcher();
 
-        return 0;
-    }
+    // Create new models
+    \App\Models\Employee::factory()->count(rand(6, 45))
+      ->has(\App\Models\Timesheet::factory()->count(rand(1, 8))
+          ->has(\App\Models\TimesheetDay::factory()->count(rand(1, 22))))
+      ->has(\App\Models\Leave::factory()->count(rand(1, 3)))
+      ->create();
+
+    return 0;
+  }
 }
